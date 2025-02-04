9810f8a62de34d884c029e96f5271ba0066bb2db
Cyber-cube <cyber.cube6198@gmail.com>
2025-02-04 18:03:42 +0530

    Added more things

--- src/UI.jsx
+++ src/UI.jsx
@@ -2,8 +2,8 @@ import './UI.css'
 import { useState } from 'react'
 
 function UI() {
-  const [x, setX] = useState()
-  const [y, setY] = useState()
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
 
   return (
     <div className="main">
@@ -11,7 +11,7 @@ function UI() {
         <p className='text'>Meow mew me me meow meow m?</p>
       </div>
       <div className='btn-div'>
-        <button className='btn'>Yes</button>
+        <button className='btn yes'>Yes</button>
         <button onClick={() => {
           setY(Math.floor(Math.random() * 151))
           setX(Math.floor(Math.random() * 151))
